c983cbde4ee01ba3aefb09ce74cff1e433b8e22a
Jessica <jessicacotrina@gmail.com>
2020-11-30 12:20:20 -0500

    model for data base

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require("./user");
+const Category = require("./category");
+const Transaction = require("./transaction");
+const Budget = require("./budget");
+
+// associations here
+
+module.exports = {
+  User,
+  Category,
+  Transaction,
+  Budget,
+};
