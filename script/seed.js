af5507c87762da54b470b4534a3ac740abd208ac
Torrel <taurel107@gmail.com>
2020-12-02 10:51:03 -0500

    feat(seed file) updated seed file

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,93 @@
-const db = require("../server/db");
-const { Budget, User, Category, Transaction } = require("../server/db");
+const { green, red } = require("chalk");
+//const db = require("../server/db");
+const { db, Budget, User, Category, Transaction } = require("../server/db");
 
 async function seed() {
-  const users = await Promise.all([
-    User.create({
+ try {
+    await db.sync({ force: true });
+    const users = await Promise.all([
+      User.create({
       firstName: "Jessica",
       lastName: "Cotrina",
       email: "jessi@gmail.com",
       password: "1234",
     }),
+    User.create({
+      firstName: "Julissa",
+      lastName: "Napoletano",
+      email: "julissa@gmail.com",
+      password: "1234",
+    }),
+    User.create({
+      firstName: "Cathy",
+      lastName: "Sun",
+      email: "cathy@gmail.com",
+      password: "1234",
+    }),
+    User.create({
+      firstName: "Torrel",
+      lastName: "Jeremiah",
+      email: "torrel@gmail.com",
+      password: "1234",
+    }),
+    User.create({
+      firstName: "Yuliya",
+      lastName: "Maroz",
+      email: "yuliya@gmail.com",
+      password: "1234",
+    }),
   ]);
-
+ 
   const budgets = await Promise.all([
     Budget.create({
       amount: 100,
       period: "monthly",
     }),
+    Budget.create({
+      amount: 500,
+      period: "monthly",
+    }),
+    Budget.create({
+      amount: 1000,
+      period: "monthly",
+    }),
+    Budget.create({
+      amount: 300,
+      period: "monthly",
+    }),
   ]);
-
-  const categories = await Promise.all([
+ 
+const categories = await Promise.all([
     Category.create({
-      name: "groceries",
+      name: "Groceries",
       imageUrl:
         "https://www.flaticon.com/svg/static/icons/svg/2921/2921829.svg",
     }),
+    Category.create({
+      name: "Mortage",
+      imageUrl:
+        "https://www.flaticon.com/svg/static/icons/svg/1040/1040988.svg"
+
+    }),
+    Category.create({
+      name: "Food & Dining",
+      imageUrl:
+       "https://www.flaticon.com/svg/static/icons/svg/1999/1999067.svg"
+        
+    }),
+    Category.create({
+      name: "Home Phone",
+      imageUrl: "https://www.flaticon.com/svg/static/icons/svg/2829/2829733.svg"
+
+    }),
+    Category.create({
+      name: "Utilities",
+      imageUrl: "https://www.flaticon.com/svg/static/icons/svg/270/270644.svg"
+    }), 
+    Category.create({
+      name: "Entertainment",
+      imageUrl: "https://www.flaticon.com/svg/static/icons/svg/3163/3163508.svg"
+    })   
   ]);
 
   const transactions = await Promise.all([
@@ -31,7 +95,50 @@ async function seed() {
       amount: 50,
       storeName: "wallmart",
     }),
-  ]);
+    Transaction.create({
+      amount: 100,
+      storeName: "target",
+    }),
+    Transaction.create({
+      amount: 200,
+      storeName: "homedepot",
+    }),
+    Transaction.create({
+      amount: 200,
+      storeName: "staples",
+    }),
+    Transaction.create({
+      amount: 200,
+      storeName: "BP Gas",
+    }),
+    Transaction.create({
+      amount: 50,
+      storeName: "wallmart",
+    }),
+    Transaction.create({
+      amount: 50,
+      storeName: "wallmart",
+    }),
+    Transaction.create({
+      amount: 50,
+      storeName: "wallmart",
+    }),
+    Transaction.create({
+      amount: 50,
+      storeName: "wallmart",
+    }),
+    Transaction.create({
+      amount: 50,
+      storeName: "wallmart",
+    }),
+    Transaction.create({
+      amount: 50,
+      storeName: "wallmart",
+    }),
+ ]);
+} catch (err){
+    console.log(red(err))
+  }
 }
 
 async function runSeed() {
@@ -39,7 +146,7 @@ async function runSeed() {
   try {
     await seed();
   } catch (err) {
-    // console.error(err);
+    console.error(err);
     process.exitCode = 1;
   } finally {
     console.log("closing db connection");
--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,33 @@
-const Sequelize = require("sequelize");
+//const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/capstone",
-  {
-    logging: false,
-  }
-);
+const chalk = require('chalk')
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
 
-module.exports = db;
+
+//new code
+
+const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
+console.log(chalk.yellow(`Opening database connection to ${dbName}`))
+
+const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+  logging: false,
+})
+
+module.exports = db
+
+
+
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/capstone",
+//   {
+//     logging: false,
+//   }
+// );
+
+// module.exports = db;
+
 
 // if (process.env.NODE_ENV === "test") {
 //   after("close database connection", () => db.close());
